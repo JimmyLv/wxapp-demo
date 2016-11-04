80da4bdf7c439a18cb8440e5ccbefe17f3d6382c
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-11-04 19:26:04 +0800

    [#1] - Just fixed incorrect icon path.

--- utils/util.js
+++ utils/util.js
@@ -1,17 +1,16 @@
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
 export function formatTime(date) {
   var year = date.getFullYear()
   var month = date.getMonth() + 1
   var day = date.getDate()
-
+  
   var hour = date.getHours()
   var minute = date.getMinutes()
   var second = date.getSeconds()
-
-
+  
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
 }
